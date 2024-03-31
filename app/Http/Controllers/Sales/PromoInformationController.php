2d076fffdd4b08d8aadb15c27585dd867be5a30b
KingJB13 <justin.bais13@gmail.com>
2024-03-31 22:49:18 +0800

    Fixed Promo Controller conditions

--- app/Http/Controllers/Sales/PromoInformationController.php
+++ app/Http/Controllers/Sales/PromoInformationController.php
@@ -13,7 +13,7 @@ class PromoInformationController extends Controller
         $promo = Promo::join('products', 'promos.product_id', '=', 'products.id')
                         ->select('promos.code as code', 'promos.percent as discount', 'promos.from_date as start', 'promos.till_date as end', 'products.name as product')
                         ->where('promos.till_date','>=',now())
-                        ->orWhere('promos.from_date','<=', now())
+                        ->where('promos.from_date','<=', now())
                         ->paginate(10);
 
         return  view('layouts.sales.promoinformation', ['promo' => $promo])->with('i',(request()->input('page', 1) -1) *10);
