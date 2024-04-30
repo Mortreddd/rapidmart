1894b0b82bda2bd6cba7a62b522da7116b2dc558
Maku <manansalamarkerol@gmail.com>
2024-05-01 06:00:37 +0800

    change the directory from PO to INVENTORY

--- app/Http/Controllers/Sales/CheckInventoryController.php
+++ app/Http/Controllers/Sales/CheckInventoryController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Sales; 
+namespace App\Http\Controllers\Sales;
 
 use App\Http\Controllers\Controller;
-use App\Models\PO\Product;
+use App\Models\Inventory\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,39 +11,40 @@ class CheckInventoryController extends Controller
 {
     public function index()
     {
-            $products = Product::join('catergories','products.catergory_id','=','catergories.id')
-                                ->select('products.id as product_id',
-                                'products.name as product_name',
-                                'catergories.name as category_name',
-                                'products.price as price',
-                                'products.stocks as stock'
-                                )
-                                ->paginate(10);
-        return view('layouts.sales.checkinventory',['products' => $products])->with('i',(request()->input('page', 1) -1) *10);
+        $products = Product::join('catergories', 'products.catergory_id', '=', 'catergories.id')
+            ->select(
+                'products.id as product_id',
+                'products.name as product_name',
+                'catergories.name as category_name',
+                'products.price as price',
+                'products.stocks as stock'
+            )
+            ->paginate(10);
+        return view('layouts.sales.checkinventory', ['products' => $products])->with('i', (request()->input('page', 1) - 1) * 10);
     }
 
-    public function search(Request $request){
+    public function search(Request $request)
+    {
         $request->validate([
-            'search' => 'nullable|string|max:50', 
+            'search' => 'nullable|string|max:50',
         ]);
 
         $searchTerm = $request->input('search');
 
-        if(!empty($searchTerm))
-        {
-            $products = Product::join('catergories','products.catergory_id','=','catergories.id')
-                                ->select(
-                                    'products.id as product_id',
-                                    'products.name as product_name',
-                                    'catergories.name as category_name',
-                                    'products.price as price',
-                                    'products.stocks as stock'
-                                )
-                                ->where('products.name','LIKE','%'.$searchTerm.'%')
-                                ->orWhere('catergories.name','LIKE', '%' .$searchTerm. '%')
-                                ->paginate(10);
+        if (!empty($searchTerm)) {
+            $products = Product::join('catergories', 'products.catergory_id', '=', 'catergories.id')
+                ->select(
+                    'products.id as product_id',
+                    'products.name as product_name',
+                    'catergories.name as category_name',
+                    'products.price as price',
+                    'products.stocks as stock'
+                )
+                ->where('products.name', 'LIKE', '%' . $searchTerm . '%')
+                ->orWhere('catergories.name', 'LIKE', '%' . $searchTerm . '%')
+                ->paginate(10);
         }
-        return view('layouts.sales.checkinventory',['products' => $products])->with('i',(request()->input('page', 1) -1) *10);
+        return view('layouts.sales.checkinventory', ['products' => $products])->with('i', (request()->input('page', 1) - 1) * 10);
 
     }
 }
